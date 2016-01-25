d0edb73db3ed541742da5abc8a31d4da469da234
Zhantao Peng <peng.z@husky.neu.edu>
2016-01-25 18:47:43 -0500

    Zhantao Assignment1

--- sketch.js
+++ sketch.js
@@ -18,9 +18,26 @@
 */
 
 function setup() {
-
+  createCanvas(500,500)
+  background(0,76,153)
 }
 
 function draw() {
-
+  //object setup
+  fill(153,153,255)
+  strokeWeight(10)
+  stroke(255,255,204);
+  
+  //loop
+  var rectX = 0;
+  while (rectX <= width){
+  rect(rectX,rectY,50,50)
+  rectX = rectX + 70
+    var rectY = 0;
+    while (rectY <= height){
+      rect(rectX,rectY,50,50)
+      rectY = rectY + 70
+    }
+  }
+  
 }
